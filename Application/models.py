f9cbe166b575a6cc067da68a5a8c7363a2a6e669
Jorge Rivero <riverojorge145@gmail.com>
2018-10-31 18:21:57 -0500

    Added unittesting for User model

--- Application/models.py
+++ Application/models.py
@@ -7,8 +7,8 @@ def load_user(user_id):
 	return User.query.get(int(user_id))
 
 friends = db.Table('friends', 
-	db.Column('self_id', db.Integer, db.ForeignKey(user.id)), 
-	db.Column('friend_id', db.Integer, db.ForeignKey(user.id))
+	db.Column('self_id', db.Integer, db.ForeignKey('user.id')), 
+	db.Column('friend_id', db.Integer, db.ForeignKey('user.id'))
 	)
 
 
@@ -18,11 +18,22 @@ class User(db.Model, UserMixin):
 	email = db.Column(db.String(120), unique=True, nullable=False)
 	password = db.Column(db.String(60), nullable=False)
 	games_won = db.Column(db.Integer(), nullable=False)
-	friends = db.relationship(
+	friend_list = db.relationship(
 		"User", secondary=friends,
 		primaryjoin = (friends.c.self_id == id),
 		secondaryjoin = (friends.c.friend_id == id), 
 		backref= db.backref('friends', lazy='dynamic'), lazy='dynamic')
+
+	def add_friend(self, user):
+		if not self.is_friends(user):
+			self.friends.append(user)
+
+	def remove_friend(self, user):
+		if self.is_friends(user):
+			self.friends.remove(user)
+
+	def is_friends(self, user):
+		return self.friend_list.filter(friends.c.self_id == user.id).count() > 0 
 
 	def get_reset_token(self, expires_sec=1800):
 		s = Serializer(application.config['SECRET_KEY'], expires_sec)
---
+++ tests.py
@@ -0,0 +1,34 @@
+import unittest
+from Application import application, db
+from Application.models import User, friends
+
+class UserModel(unittest.TestCase):
+
+	def setUp(self):
+		application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+		db.create_all()
+
+	def tearDown(self):
+		db.session.remove()
+		db.drop_all()
+
+	def test_add_friend(self):
+		u1 = User(username="Player1", email="ayy@lmao.com", password="cat", games_won=0)
+		u2 = User(username="Player2", email="me@me.com", password="dog", games_won=0)
+		db.session.add(u1)
+		db.session.add(u2)
+		db.session.commit()
+		self.assertEqual(u1.friend_list.all(), [])
+		self.assertEqual(u2.friend_list.all(), [])
+
+		u1.add_friend(u2)
+		db.session.commit()
+		self.assertTrue(u1.is_friends(u2))
+		self.assertEqual(u1.friends.count(), 1)
+		self.assertEqual(u1.friends.first().username, "Player2")
+		self.assertEqual(u2.friends.count(), 1)
+		self.assertEqual(u2.friends.first().username, "Player1")
+
+		u1.remove_friend(u2)
+		db.session.commit()
+		self.assertFalse(u1.is_friends(u2)) 
